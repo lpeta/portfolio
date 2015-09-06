527fc0b453d061482f76a9e62476165a1cc6529d
Leonardo Peta <leo@Leonardos-MacBook-Pro.local>
2015-09-06 21:51:59 +0100

    Adding an initial layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,39 @@
 
   'use strict';
 
-  console.log('Allo Allo');
+  $('#send-email').on('submit', function(e) {
+    e.preventDefault();
+    // check if everything is fine
+    // if it is not fine, then hightlight the items in the page
+    // otherwise send the email
+    showLoader();
+    sendEmail($('#name').val(), $('#email').val(), $('#message').val(), hideLoader);
+  });
+
+  function hideLoader() {
+    $('.progress-wrap').addClass('hide');
+  }
+
+  function showLoader() {
+    $('.progress-wrap').removeClass('hide');
+  }
+
+  function sendEmail(nameIn, emailIn, messageIn, callback) {
+    $.ajax({
+      url: '//formspree.io/leonardopeta@live.co.uk', 
+      method: 'POST',
+      data: {
+        name: nameIn,
+        email: emailIn,
+        message: messageIn
+      },
+      dataType: 'json'
+    })
+    .success(function() {
+      setTimeout(function() {
+        callback();
+      }, 500);
+    });
+  }
 
 })();
